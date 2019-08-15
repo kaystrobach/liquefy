0210169776bac89d363e6a89414473ee945b7e9d
Kay Strobach <github@kay-strobach.de>
2019-08-15 14:06:45 +0200

    Update FormViewHelper.php

--- src/ViewHelpers/FormViewHelper.php
+++ src/ViewHelpers/FormViewHelper.php
@@ -37,11 +37,28 @@ class FormViewHelper extends AbstractTagBasedViewHelper
     {
         parent::initializeArguments();
         $this->registerUniversalTagAttributes();
+        $this->registerTagAttribute('enctype', 'string', 'MIME type with which the form is submitted');
+        $this->registerTagAttribute('method', 'string', 'Transfer type (GET or POST)');
+        $this->registerTagAttribute('name', 'string', 'Name of form');
+        $this->registerTagAttribute('onreset', 'string', 'JavaScript: On reset of the form');
+        $this->registerTagAttribute('onsubmit', 'string', 'JavaScript: On submit of the form');
+        $this->registerArgument('arguments', 'array', '');
         $this->registerArgument('action', 'string', '');
         $this->registerArgument('method', 'string', '');
         $this->registerArgument('object', 'string', '');
         $this->registerArgument('objectName', 'string', '');
         $this->registerArgument('controller', 'string', '');
+        $this->registerArgument('package', 'string', '');
+        $this->registerArgument('subpackage', 'string', '');
+        $this->registerArgument('section', 'string', '');
+        $this->registerArgument('format', 'string', '');
+        $this->registerArgument('additionalParams', 'array', '');
+        $this->registerArgument('absolute', 'boolean', '');
+        $this->registerArgument('addQueryString', 'boolean', '');
+        $this->registerArgument('argumentsToBeExcludedFromQueryString', 'array', '');
+        $this->registerArgument('fieldNamePrefix', 'string', '');
+        $this->registerArgument('actionUri', 'string', '');
+        $this->registerArgument('useParentRequest', 'string', '');
     }
 
     public function render()
