7b38a369254527f751345a05a2be98c2a23b646d
Kay Strobach <github@kay-strobach.de>
2019-09-02 10:30:00 +0200

    Update TranslateViewHelper.php

--- src/ViewHelpers/TranslateViewHelper.php
+++ src/ViewHelpers/TranslateViewHelper.php
@@ -2,6 +2,7 @@
 
 namespace KayStrobach\Liquefy\ViewHelpers;
 
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
@@ -10,29 +11,6 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
  */
 class TranslateViewHelper extends AbstractViewHelper
 {
-    /**
-     * Renders the translated label.
-
-     * Replaces all placeholders with corresponding values if they exist in the
-     * translated label.
-     *
-     * @param string $id Id to use for finding translation (trans-unit id in XLIFF)
-     * @param string $value If $key is not specified or could not be resolved, this value is used. If this argument is not set, child nodes will be used to render the default
-     * @param array $arguments Numerically indexed array of values to be inserted into placeholders
-     * @param string $source Name of file with translations
-     * @param string $package Target package key. If not set, the current package key will be used
-     * @param mixed $quantity A number to find plural form for (float or int), NULL to not use plural forms
-     * @param string $locale An identifier of locale to use (NULL for use the default locale)
-     * @return string Translated label or source label / ID key
-     * @throws ViewHelperException
-     */
-    public function render($id = null, $value = null, array $arguments = array(), $source = 'Main', $package = null, $quantity = null, $locale = null)
-    {
-        if ($value !== null) {
-            return $value;
-        }
-        return $id;
-    }
 
     /**
      * Default implementation of validating additional, undeclared arguments.
@@ -47,6 +25,10 @@ class TranslateViewHelper extends AbstractViewHelper
      */
     public function validateAdditionalArguments(array $arguments)
     {
-       
+
+    }
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        return $renderChildrenClosure();
     }
 }
